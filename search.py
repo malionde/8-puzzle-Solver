14839fe6adf1b8f63fa036d3251e4882f5cd8ac8
Cem Berke Çebi <1401020016@stu.iku.edu.tr>
2018-12-11 19:24:33 +0300

    Final Version without GUI LAST

--- search.py
+++ search.py
@@ -1,24 +1,24 @@
-from collections import deque
-import numpy as np
-import sys
+from collections import deque #liste, sözlük ve tuple üzerinde build-in methodlar sağlar
+import numpy as np # Diziler üzerinde hızlı işlem yapılmasını sağlar
+import sys #sistem modüllerini içerir.
 
 class Search:
  
 
-    def __init__(self):
+    def __init__(self): #ilk değerleri initialize etmeye yarar.
         self.path_to_solution = []
         self.states = []
 
-    def bfs(self, puzzle_board):
+    def bfs(self, puzzle_board): #Bread First Search 
         global first
         first=puzzle_board
-        frontier = deque()
-        explored = set()
-        frontier.append(puzzle_board)
+        frontier = deque() #frontier adında bir deque objesi oluşturduk.
+        explored = set() # explored adında bir sırası önemli olmayan bir obje oluşturduk. 
+        frontier.append(puzzle_board) # puzzle boardda bulunan verileri sırasıyla frontier değişkenine atıyoruz. 
 
         while frontier:
-            puzzle = frontier.popleft()
-            explored.add(tuple(puzzle.puzzle_state))
+            puzzle = frontier.popleft() # popleft kullanarak puzzle board'dan eklediğimiz verileri soldan sağa doğru sırayla çıkartıyoruz.
+            explored.add(tuple(puzzle.puzzle_state)) # 
 
             if puzzle.goal_test():
                 self.path_to_solution = []
