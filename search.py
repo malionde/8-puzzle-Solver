22a59e31326aecbd10781029246a1c1852c4b49a
malionde <malionde@gmail.com>
2018-12-18 00:32:29 +0300

    Final

--- search.py
+++ search.py
@@ -1,24 +1,24 @@
-from collections import deque #liste, sözlük ve tuple üzerinde build-in methodlar sağlar
-import numpy as np # Diziler üzerinde hızlı işlem yapılmasını sağlar
-import sys #sistem modüllerini içerir.
+from collections import deque 
+import numpy as np 
+import sys 
 
 class Search:
  
 
-    def __init__(self): #ilk değerleri initialize etmeye yarar.
+    def __init__(self):
         self.path_to_solution = []
         self.states = []
 
-    def bfs(self, puzzle_board): #Bread First Search 
+    def bfs(self, puzzle_board): 
         global first
         first=puzzle_board
-        border = deque() #border adında bir deque objesi oluşturduk.
-        visited = set() # visited adında bir sırası önemli olmayan bir obje oluşturduk. 
-        border.append(puzzle_board) # puzzle boardda bulunan verileri sırasıyla border değişkenine atıyoruz. 
+        border = deque() 
+        visited = set()
+        border.append(puzzle_board) 
 
         while border:
-            puzzle = border.popleft() # popleft kullanarak puzzle board'dan eklediğimiz verileri soldan sağa doğru sırayla çıkartıyoruz.
-            visited.add(tuple(puzzle.puzzle_status)) # 
+            puzzle = border.popleft() 
+            visited.add(tuple(puzzle.puzzle_status)) 
 
             if puzzle.goal_test():
                 self.path_to_solution = []
@@ -62,22 +62,17 @@ class Search:
             parent = child.parent
             alist.append(child)
 
-            #child.print_puzzle()
-            #child.print_puzzle()
+
             self.states.append(child.state)
             path_to_solution.append(child)
             child = parent
      
 
-        #if parent.goal_test() != True:
         alist.reverse()
 
         first.print_puzzle()
-        #f = open("output2.txt", "a")
         
         for i in alist:
-            #i.print_puzzle()
             i.print_puzzle2()
 
             
-          #  f.write('OLDUUUUUUUUU: ' + i.print_puzzle() + '\n')
